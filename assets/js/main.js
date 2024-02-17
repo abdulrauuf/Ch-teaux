7c52dc8b07c01d2dcaff6fe2f2b04edfb9f31b3a
abdulrauf <156670234+abdulrauuf@users.noreply.github.com>
2024-02-17 23:28:13 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,49 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+/*====  Menu Show====*/
+// Validate if constant exists
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*====  Menu Hidden====*/
+/* Validate is constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER CLOTHING ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
 
-
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
