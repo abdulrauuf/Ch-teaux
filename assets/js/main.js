68c81052abaeef58af7ffe471f320933cc4e739f
abdulrauf <156670234+abdulrauuf@users.noreply.github.com>
2024-02-18 02:14:34 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,17 +34,46 @@ let swiperHome = new Swiper('.home__swiper', {
     grabCursor: true,
     slidesPerView: 'auto',
     
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    // },
 
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            centeredSlides: 'auto',
+        },
+        1152: {
+            centeredSlides: 'auto',
+            spaceBetween: -64,
+        },
+    }
 })
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+   //reset: true //Animation repeat
+})
+
+sr.reveal(`.home__swiper, .home__footer`)
+sr.reveal(`.home__circle`, { scale: 1.5, delay: 300 })
+sr.reveal(`.home__subcircle`, { scale: 1.5, delay: 500 })
+sr.reveal(`.home__title`, { scale: 1, origin: 'bottom', delay: 1200 })
+sr.reveal(`.swiper-button-prev, .swiper-button-next`, { origin: 'bottom' })
+
